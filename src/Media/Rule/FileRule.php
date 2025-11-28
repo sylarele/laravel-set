20dea1a72eb28d06da70ec8cb885c7b9fbcaf0b2
noelmaHelloCSE <mathieu.noel@hellocse.fr>
2025-11-28 12:06:03 +0100

    refactor(rule): use service

--- src/Media/Rule/FileRule.php
+++ src/Media/Rule/FileRule.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Sylarele\LaravelSet\Media\Rule;
 
 use BackedEnum;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Validation\Rule;
 use RuntimeException;
 use Sylarele\LaravelSet\Media\Dto\Config\FileRuleConfigDto;
+use Sylarele\LaravelSet\Media\Service\FileRuleService;
 
 class FileRule
 {
@@ -17,19 +19,21 @@ class FileRule
      */
     public static function make(BackedEnum $fileType): array
     {
-        $rule = Config::get(\sprintf('file_rules.rules.%s', $fileType->value));
+        /** @var FileRuleService $service */
+        $service = App::make(FileRuleService::class);
+        $rule = $service->find($fileType);
 
-        if (! $rule instanceof FileRuleConfigDto) {
+        if (! $rule->fileRuleDto instanceof FileRuleConfigDto) {
             throw new RuntimeException('FileRule rule already defined');
         }
 
-        $file = $rule->type === 'file'
+        $file = $rule->fileRuleDto->type === 'file'
             ? Rule::file()
             : Rule::imageFile();
 
         return [
-            $file::types($rule->mimes)
-                ->between($rule->sizeMin, $rule->sizeMax),
+            $file::types($rule->fileRuleDto->mimes)
+                ->between($rule->fileRuleDto->sizeMin, $rule->fileRuleDto->sizeMax),
         ];
     }
 }
