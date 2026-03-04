4c93302461a8cce767fb1b826d39fde0478c827b
Maxime Maisonnas <maxime.maisonnas@helloce.fr>
2026-03-04 15:36:21 +0100

    refactor(compression-level): improve doc

--- src/Media/Dto/Config/ImageCompressionLevelDto.php
+++ src/Media/Dto/Config/ImageCompressionLevelDto.php
@@ -7,7 +7,8 @@ namespace Sylarele\LaravelSet\Media\Dto\Config;
 final readonly class ImageCompressionLevelDto
 {
     /**
-     * @param int<0, 100> $sizeFrom Percentage of the total image size at which this level starts to apply
+     * @param int<0, 100> $sizeFrom Percentage of the total image size relative to the maximum image
+     * size at which this level applies
      * @param int<1, 100> $quality Quality percentage applied at this level
      */
     public function __construct(
