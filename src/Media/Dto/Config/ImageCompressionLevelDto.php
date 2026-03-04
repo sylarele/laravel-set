642f09b49d634af37f70a27466e40c28e6b68e50
Maxime Maisonnas <maxime.maisonnas@helloce.fr>
2026-03-04 16:41:43 +0100

    fix(image-compression-level): add magic method `__set_state`

--- src/Media/Dto/Config/ImageCompressionLevelDto.php
+++ src/Media/Dto/Config/ImageCompressionLevelDto.php
@@ -7,13 +7,24 @@ namespace Sylarele\LaravelSet\Media\Dto\Config;
 final readonly class ImageCompressionLevelDto
 {
     /**
-     * @param int<0, 100> $sizeFrom Percentage of the total image size relative to the maximum image
+     * @param int $sizeFrom Percentage of the total image size relative to the maximum image
      * size at which this level applies
-     * @param int<1, 100> $quality Quality percentage applied at this level
+     * @param int $quality Quality percentage applied at this level
      */
     public function __construct(
         public int $sizeFrom,
         public int $quality,
     ) {
+    }
+
+    /**
+     * @param array<string, int> $properties
+     */
+    public static function __set_state(array $properties): self
+    {
+        return new self(
+            sizeFrom: $properties['sizeFrom'],
+            quality: $properties['quality'],
+        );
     }
 }
